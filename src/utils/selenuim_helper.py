b6deb13a040b33c4dc025738238d802fb15b6ae7
ErEr <jeffliao92316046@gmail.com>
2025-01-22 22:11:40 +0800

    Feat: add selenium helper for managing Chrome driver with context manager

---
+++ src/utils/selenuim_helper.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from contextlib import contextmanager
+from selenium.webdriver.common.by import By
+
+XPATH = By.XPATH
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
+
+@contextmanager
+def get_driver(debug=False):
+    if debug:
+        driver = webdriver.Chrome()
+    else:
+        driver = webdriver.Chrome(options=options)
+    try:
+        yield driver
+    finally:
+        driver.close()
