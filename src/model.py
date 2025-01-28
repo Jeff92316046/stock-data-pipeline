a6270c5057f763ee89443ff96271a69daccb9de5
ErEr <jeffliao92316046@gmail.com>
2025-01-28 22:55:27 +0800

    Feat: use SQLModel insteal of sqlalchemy model

--- src/model.py
+++ src/model.py
@@ -1,41 +1,31 @@
-import json
-from utils.db_helper import Base
-from sqlalchemy import (
-    String,
-    Integer,
-    UnicodeText,
-    Date,
-    ForeignKey,
-    Column,
-    BIGINT,
-    UniqueConstraint,
-)
-from sqlalchemy.inspection import inspect
+from sqlmodel import TEXT, String, SQLModel, Field, Relationship,BIGINT,Column
+from datetime import date
+from sqlalchemy import UniqueConstraint,Column
 
 
-class HelperMixin:
-    def to_dict(self):
-        return {c.key: getattr(self, c.key) for c in inspect(self).mapper.column_attrs}
-
-
-class StockList(Base, HelperMixin):
+class StockList(SQLModel, table=True):  # 加上 table=True 表示這是一個數據表
     __tablename__ = "stock_list"
-    stock_symbol = Column(String(length=20), primary_key=True, nullable=False)
-    stock_name = Column(UnicodeText(), nullable=True)
-    last_updated_at = Column(Date(), nullable=True)
+    stock_symbol: str = Field(primary_key=True, max_length=20,sa_type=String)
+    stock_name: str | None = Field(default=None,sa_type=TEXT)
+    last_updated_at: date | None = Field(default=None)
+
+    distributions: list["StockShareDistribution"] = Relationship(back_populates="stock")
 
 
-class StockShareDistribution(Base, HelperMixin):
+class StockShareDistribution(SQLModel, table=True):
     __tablename__ = "stock_share_distribution"
-    id = Column(Integer(), primary_key=True, nullable=False, autoincrement=True)
-    stock_symbol = Column(String(length=20), ForeignKey("stock_list.stock_symbol"))
-    date_time = Column(Date())
-    holding_order = Column(Integer())
-    number_of_holder = Column(Integer())
-    shares = Column(BIGINT())
-    created_at = Column(Date())
+    id: int | None = Field(default=None, primary_key=True)
+    stock_symbol: str = Field(foreign_key="stock_list.stock_symbol", max_length=20,sa_type=String)
+    date_time: date = Field(default=None)
+    holding_order: int = Field(default=None)
+    number_of_holder: int | None = Field(default=None)
+    shares: int | None = Field(default=None,sa_column=Column(BIGINT))
+    created_at: date | None = Field(default=None)
+
     __table_args__ = (
         UniqueConstraint(
             "stock_symbol", "date_time", "holding_order", name="uix_stock_distribution"
         ),
     )
+
+    stock: StockList = Relationship(back_populates="distributions")
