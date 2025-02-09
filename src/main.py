7c0377e09438202ce2a9247890d25f641a32f231
ErEr <jeffliao92316046@gmail.com>
2025-02-09 21:54:24 +0800

    Feat: add main flow for updating stock list and data using Prefect

---
+++ src/main.py
@@ -0,0 +1,11 @@
+from scraping.get_stock_data import update_all_stocksd_data
+from scraping.update_stock_list import update_stock_list
+from prefect import flow
+
+@flow
+def main():
+    update_stock_list()
+    update_all_stocksd_data()
+
+if __name__ == "__main__":
+    main()
