4bcb195ad2c7bd61469c31d9988637726e6af3ae
ErEr <jeffliao92316046@gmail.com>
2025-02-09 21:54:05 +0800

    Refactor: streamline stock repository functions and improve logging

--- src/repository/stock_list_repository.py
+++ src/repository/stock_list_repository.py
@@ -8,39 +8,30 @@ from sqlalchemy.engine.cursor import CursorResult
 from utils.db_helper import get_db, engine
 
 
-@task
 def get_all_stock():
     session = next(get_db())
-    stmt = select(Stocks)
+    stmt = select(Stocks).order_by(Stocks.stock_symbol)
     stock_list = session.exec(stmt).all()
     return stock_list
 
-
-@task
 def get_all_stock_symbol():
     session = next(get_db())
     stmt = select(Stocks.stock_symbol)
     stock_list = session.exec(stmt).all()
     return stock_list
 
-
-@task
 def get_stock_by_symbol(stock_symbol):
     session = next(get_db())
     stmt = select(Stocks).where(Stocks.stock_symbol == stock_symbol)
     stock = session.exec(stmt).one()
     return stock
 
-
-@task
 def get_stock_last_updated_date_by_symbol(stock_symbol):
     session = next(get_db())
     stmt = select(Stocks).where(Stocks.stock_symbol == stock_symbol)
     stock = session.exec(stmt).one()
     return stock.last_updated_at
 
-
-@task
 def upsert_stock_by_symbol(stock_symbol, stock_name):
     session = next(get_db())
     stmt = postgresql.insert(Stocks).values(
@@ -48,16 +39,13 @@ def upsert_stock_by_symbol(stock_symbol, stock_name):
     )
     stmt = stmt.on_conflict_do_nothing(index_elements=["stock_symbol"])
     result: CursorResult = session.exec(stmt)
-    if result.rowcount == 1:
-        get_run_logger().info(f"Stock {stock_symbol} stock name {stock_name} inserted")
     session.commit()
+    return result.rowcount
 
-
-@task
-def update_stock_date_by_symbol(stock_symbol, last_updated_at):
+def upsert_stock_date_by_symbol(stock_symbol, last_updated_at):
     session = next(get_db())
-    statment = select(Stocks).where(Stocks.stock_symbol == stock_symbol)
-    stock = session.exec(statment).one()
-    stock.last_updated_at = last_updated_at
-    session.commit()
-    get_run_logger().info(f"Stock {stock_symbol} last updated at {last_updated_at}")
+    statement = select(Stocks).where(Stocks.stock_symbol == stock_symbol)
+    stock = session.exec(statement).first()
+    if stock.last_updated_at is None or last_updated_at > stock.last_updated_at:
+        stock.last_updated_at = last_updated_at
+        session.commit()
--- src/repository/stock_share_distribution_repository.py
+++ src/repository/stock_share_distribution_repository.py
@@ -1,14 +1,10 @@
 from model import StockShareDistribution as stockSD
-from prefect import task
 from prefect.logging import get_run_logger
 from utils.db_helper import get_db
-from sqlmodel import insert, select
 from sqlalchemy.dialects import postgresql
 from sqlalchemy.engine.cursor import CursorResult
-from sqlalchemy.orm import Session
 
 
-@task
 def upsert_stock_share_distribution(
     stock_symbol, date_time, holding_order, number_of_holder, shares, created_at
 ):
@@ -27,33 +23,17 @@ def upsert_stock_share_distribution(
         index_elements=["stock_symbol", "date_time", "holding_order"]
     )
     result: CursorResult = session.exec(stmt)
-    if result.rowcount == 0:
-        get_run_logger().warning(
-            f"Stock {stock_symbol} date {date_time} holding order {holding_order} already exists"
-        )
     session.commit()
+    return result.rowcount
 
-
-# @task
-def upsert_stock_share_distributions(stock_SDs: list[stockSD]):
+def upsert_stock_share_distributions(stock_sds: list[stockSD]):
     session = next(get_db())
     stmt = postgresql.insert(stockSD).values(
-        [stock_SD.model_dump(exclude_none=True) for stock_SD in stock_SDs]
+        [stock_sd.model_dump(exclude={"id"}) for stock_sd in stock_sds]
     )
     stmt = stmt.on_conflict_do_nothing(
         index_elements=["stock_symbol", "date_time", "holding_order"]
     )
     result: CursorResult = session.exec(stmt)
     session.commit()
-    if result.rowcount == 0:
-        get_run_logger().warning(
-            f"Stock {stock_SDs[0].stock_symbol} date {stock_SDs[0].date_time} already exists"
-        )
-    elif result.rowcount == len(stock_SDs):
-        get_run_logger().info(
-            f"Stock {stock_SDs[0].stock_symbol} date {stock_SDs[0].date_time} inserted"
-        )
-    else:
-        get_run_logger().warning(
-            f"Stock {stock_SDs[0].stock_symbol} date {stock_SDs[0].date_time} some inserted {result.rowcount} of {len(stock_SDs)}"
-        )
+    return result.rowcount
