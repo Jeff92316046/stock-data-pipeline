04acabad97f2d032ff3eb3962495c3c880a0d51c
ErEr <jeffliao92316046@gmail.com>
2025-03-04 12:14:37 +0800

    Refactor: simplify get_driver function by removing context manager and returning driver directly

--- src/crawler/utils/selenuim_helper.py
+++ src/crawler/utils/selenuim_helper.py
@@ -1,6 +1,5 @@
 import os
 from selenium import webdriver
-from contextlib import contextmanager
 from selenium.webdriver.common.by import By
 
 XPATH = By.XPATH
@@ -10,8 +9,6 @@ options = webdriver.ChromeOptions()
 options.add_argument("--headless")
 MODE = os.getenv("MODE")
 
-
-@contextmanager
 def get_driver(debug=False):
     if debug:
         driver = webdriver.Chrome()
@@ -23,7 +20,6 @@ def get_driver(debug=False):
             )
         elif MODE == "dev":
             driver = webdriver.Chrome(options=options)
-    try:
-        yield driver
-    finally:
-        driver.quit()
+    return driver
+
+driver = get_driver()
