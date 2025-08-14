43989d6a1b7bc495b48c48f66fe045687f1b8666
ErEr <jeffliao92316046@gmail.com>
2025-08-14 21:32:49 +0800

    fix: remote webdriver session error

--- src/crawler/scraping/get_broker_trade_daily.py
+++ src/crawler/scraping/get_broker_trade_daily.py
@@ -26,75 +26,71 @@ URL = "https://bsr.twse.com.tw/bshtm/bsMenu.aspx"
 @task
 def get_stock_broker_trade_daily_in_watchlist():
     stock_list = get_all_broker_trade_watchlist()
-    with get_driver() as driver:
-        for stock_symbol in stock_list:
-            fetch_single_broker_trade_daily(driver, stock_symbol)
-
+    for stock_symbol in stock_list:
+        fetch_single_broker_trade_daily(stock_symbol)
 
 @task(cache_policy=NO_CACHE, retries=10)
 def fetch_single_broker_trade_daily(
-    driver: LocalWebDriver | RemoteWebDriver, stock_symbol: str
+    stock_symbol: str
 ):
-    try:
-        driver.get(URL)
-        driver.implicitly_wait(5)
+    with get_driver() as driver:
+        try:
+            driver.get(URL)
+            driver.implicitly_wait(5)
 
-        img = driver.find_element(
-            XPATH,
-            '//*[@id="Panel_bshtm"]/table/tbody/tr/td/table/tbody/tr[1]/td/div/div[1]/img',
-        )
-        img_bytes = img.screenshot_as_png
-        img_array = np.frombuffer(img_bytes, np.uint8)
-        img_mat = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-
-        predicted_string = ocr_captcha_onnx(img_mat)
-
-        captcha_input = driver.find_element(
-            XPATH,
-            '//*[@id="Panel_bshtm"]/table/tbody/tr/td/table/tbody/tr[1]/td/div/div[2]/input',
-        )
-        captcha_input.clear()
-        captcha_input.send_keys(predicted_string)
-        print(f"[DEBUG] Captcha: {predicted_string}")
-        time.sleep(2)
-
-        stock_symbol_inputbox = driver.find_element(XPATH, '//*[@id="TextBox_Stkno"]')
-        stock_symbol_inputbox.clear()
-        stock_symbol_inputbox.send_keys(stock_symbol)
-        time.sleep(2)
-
-        driver.find_element(XPATH, '//*[@id="btnOK"]').click()
-        time.sleep(3)
-
-        error_label = driver.find_element(ID, "Label_ErrorMsg").text.strip()
-        if error_label == "查無資料":
-            get_run_logger().info(
-                f"stock {stock_symbol} date {date.today()} data not exist"
+            img = driver.find_element(
+                XPATH,
+                '//*[@id="Panel_bshtm"]/table/tbody/tr/td/table/tbody/tr[1]/td/div/div[1]/img',
             )
-            return
+            img_bytes = img.screenshot_as_png
+            img_array = np.frombuffer(img_bytes, np.uint8)
+            img_mat = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
 
-        stock_data_link = driver.find_element(ID, "HyperLink_DownloadCSV")
-        download_url = stock_data_link.get_attribute("href")
+            predicted_string = ocr_captcha_onnx(img_mat)
 
-        cookies = {c["name"]: c["value"] for c in driver.get_cookies()}
-
-        resp = requests.get(download_url, cookies=cookies)
-
-        result = parse_broker_trade_daily(
-            resp.content, stock_symbol=stock_symbol, trade_date=date.today()
-        )
-        if result == 0:
-            get_run_logger().warning(
-                f"stock {stock_symbol} date {date.today()} is already exists"
+            captcha_input = driver.find_element(
+                XPATH,
+                '//*[@id="Panel_bshtm"]/table/tbody/tr/td/table/tbody/tr[1]/td/div/div[2]/input',
             )
-        else:
-            get_run_logger().info(f"stock {stock_symbol} date {date.today()} inserted")
+            captcha_input.clear()
+            captcha_input.send_keys(predicted_string)
+            print(f"[DEBUG] Captcha: {predicted_string}")
+            time.sleep(2)
 
-    except NoSuchElementException:
-        get_run_logger().error(f"ocr error {stock_symbol}, retrying...")
-        raise NoSuchElementException("ocr error")
-    except Exception as e:
-        get_run_logger().error(f"Skipping {stock_symbol}\n{e}")
+            stock_symbol_inputbox = driver.find_element(XPATH, '//*[@id="TextBox_Stkno"]')
+            stock_symbol_inputbox.clear()
+            stock_symbol_inputbox.send_keys(stock_symbol)
+            time.sleep(2)
+
+            driver.find_element(XPATH, '//*[@id="btnOK"]').click()
+            time.sleep(3)
+
+            error_label = driver.find_element(ID, "Label_ErrorMsg").text.strip()
+            if error_label == "查無資料":
+                get_run_logger().info(
+                    f"stock {stock_symbol} date {date.today()} data not exist"
+                )
+                return
+
+            stock_data_link = driver.find_element(ID, "HyperLink_DownloadCSV")
+            download_url = stock_data_link.get_attribute("href")
+
+            cookies = {c["name"]: c["value"] for c in driver.get_cookies()}
+
+            resp = requests.get(download_url, cookies=cookies)
+
+            result = parse_broker_trade_daily(
+                resp.content, stock_symbol=stock_symbol, trade_date=date.today()
+            )
+            if result == 0:
+                get_run_logger().warning(
+                    f"stock {stock_symbol} date {date.today()} is already exists"
+                )
+            else:
+                get_run_logger().info(f"stock {stock_symbol} date {date.today()} inserted")
+        except NoSuchElementException:
+            get_run_logger().error(f"ocr error {stock_symbol}, retrying...")
+            raise NoSuchElementException("ocr error")
 
 
 def parse_broker_trade_daily(file_bytes: bytes, stock_symbol: str, trade_date: date):
