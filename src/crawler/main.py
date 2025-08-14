8aa5cb4f6378b80361ed646b6a90124570893a60
ErEr <jeffliao92316046@gmail.com>
2025-08-14 20:58:57 +0800

    fix: revise flow name for compatibility

--- src/crawler/main.py
+++ src/crawler/main.py
@@ -1,10 +1,10 @@
 from crawler.scraping.get_stock_share_distribution import get_all_stocksd_data
 from crawler.scraping.get_broker_trade_daily import get_stock_broker_trade_daily_in_watchlist
-from scraping.update_stock_list import update_stock_list
+from crawler.scraping.update_stock_list import update_stock_list
 from prefect import flow, serve
 
 @flow
-def stock_share_distribution():
+def main():
     update_stock_list()
     get_all_stocksd_data()
 
@@ -14,6 +14,6 @@ def broker_trade_daily():
 
 if __name__ == "__main__":
     serve(
-        stock_share_distribution.to_deployment(name="stock_share_distribution", cron="0 0 * * 7"),
-        broker_trade_daily.to_deployment(name="broker_trade_daily",cron="0 8 * * *"),
+        main.to_deployment(name="stock_crawler", cron="0 0 * * 7"),
+        broker_trade_daily.to_deployment(name="broker_trade_daily",cron="30 9 * * *"),
     )
